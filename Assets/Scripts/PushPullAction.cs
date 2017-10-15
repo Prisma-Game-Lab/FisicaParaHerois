c8df04527670ac2b942fbcf8d044a6e118c3de18
Renan da Fonte <renandafonte@hotmail.com>
2017-10-15 01:02:39 -0200

    Implementação de PushPullAction
    - Do() aplica o float recebido como uma força para a direita (para mover para o outro lado, basta passar um valor negativo)

--- Assets/Scripts/PushPullAction.cs
+++ Assets/Scripts/PushPullAction.cs
@@ -8,10 +8,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PushPullAction : IAction<float> {
+public class PushPullAction : MonoBehaviour, IAction<float> {
+    private PhysicsObject _object;
+
+    void Start()
+    {
+        _object = gameObject.GetComponent<PhysicsObject>();
+    }
 
     public void Do(float temp)
     {
-        //temporary parameter and implementation
+        _object.physicsData.AddForce(temp * Vector2.right);
     }
 }
