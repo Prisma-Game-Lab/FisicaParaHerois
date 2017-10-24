e4aabd62c4e69913bd887c7ec976b8d6e58f9f1b
Daniel Oliveira <thedannyvegas@gmail.com>
2017-10-24 03:49:31 -0400

    TimelineTest scene adicionada

---
+++ Assets/ParticlesControl.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticlesControl : MonoBehaviour {
+
+	public ParticleSystem particleSystem;
+
+	public void Play(){
+
+		particleSystem.Play ();
+
+	}
+
+	public void Stop(){
+
+		particleSystem.Stop ();
+
+	}
+
+}
---
+++ Assets/timelineTrigger.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class timelineTrigger : MonoBehaviour {
+
+	public PlayableDirector playableDirector;
+
+	public void Play(){
+
+		playableDirector.Play ();
+
+	}
+}
