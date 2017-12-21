bd7acf0e6a1a04c3e0317511aab6b4142d60454a
Renan da Fonte <renandafonte@hotmail.com>
2017-12-21 16:26:12 -0200

    Splash screen leva direto para o mapa se o personagem já estiver criado
    - Criados métodos StartGameButton e CustomizationSceneOkButton no arquivo SceneManaging.cs
    - Botão na scene de customização agora chama CustomizationSceneOkButton
    - Botão de Iniciar na scene de Splash agora chama StartGameButton
    - Scene Mapa adicionada ao Build Settings

--- Assets/Scripts/SceneManaging.cs
+++ Assets/Scripts/SceneManaging.cs
@@ -10,4 +10,50 @@ public class SceneManaging : MonoBehaviour {
         SceneManager.LoadScene(s);
     }
 
+    /// <summary>
+    /// Usada no botão de iniciar
+    /// </summary>
+    public void StartGameButton(string customizationScene, string mapScene)
+    {
+        //Lê de um arquivo gerado pelo Unity se o personagem já foi criado
+        int characterCreated = PlayerPrefs.GetInt("CharacterCreated", 0);
+
+        //Se não, vai para a cena de customização
+        if(characterCreated == 0)
+        {
+            LoadScene(customizationScene);
+        }
+
+        //Se foi, vai para a cena do mapa
+        else
+        {
+            LoadScene(mapScene);
+        }
+
+        return;
+    }
+
+    /// <summary>
+    /// Usada no botão de iniciar
+    /// </summary>
+    /// <param name="scenes">Nome das cenas separado por vírgula</param>
+    public void StartGameButton(string scenes)
+    {
+        //Separa a string passada em 2 (o inspector não aceita métodos com 2 variáveis)
+        string customizationScene = scenes.Substring(0, scenes.IndexOf(','));
+        string mapScene = scenes.Substring(scenes.IndexOf(',') + 1);
+
+        StartGameButton(customizationScene, mapScene);
+    }
+
+    /// <summary>
+    /// Usada no botão da cena de customização
+    /// </summary>
+    public void CustomizationSceneOkButton(string scene)
+    {
+        //Salva em um arquivo gerado pelo Unity que o personagem já foi criado
+        PlayerPrefs.SetInt("CharacterCreated", 1);
+
+        LoadScene(scene);
+    }
 }
