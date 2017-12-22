08d77385d916ba6950a4011374cd94cdda3d6855
Nelson Donato <nelsinhojd@hotmail.com>
2017-12-22 12:44:57 -0200

    Commit do resto dos assets

---
+++ Assets/StreamingAssets/jsonMaps/tiletest.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="teste" tilewidth="140" tileheight="140" tilecount="10" columns="1">
+ <image source="../../../Tilesets/tileset-01.png" trans="ffffff" width="140" height="1400"/>
+</tileset>
