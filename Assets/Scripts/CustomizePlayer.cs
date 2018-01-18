f1ebab719c060ef66498d4444bfe3792924122e6
Michelle Beadle <michellesantiagobeadle@hotmail.com>
2018-01-18 13:19:37 -0200

    Revert "Protótipo da tela de customização feito"
    This reverts commit 910086e56055b9d88598c70824b279d4d5eb34b4.

--- Assets/Scripts/CustomizePlayer.cs
+++ Assets/Scripts/CustomizePlayer.cs
@@ -12,12 +12,6 @@ public class CustomizePlayer : MonoBehaviour {
 	public Button[] Categories;
 	public Button[] Color;
 
-	private Text _textoGenerico;
-	private int _NumInText = 0;
-	private int _NumInSymbol = 0;
-	private string[] _Symbols = {"", "!", "@", "#", "$", "%", "ˆ", "&", "*", "(", ")"};
-	private string originalText = "CUSTOM TEXT";
-
 	// Use this for initialization
 	void Start () {
 
@@ -26,20 +20,8 @@ public class CustomizePlayer : MonoBehaviour {
 		Left.onClick.AddListener (LeftButton);
 		Right.onClick.AddListener (RightButton);
 
-		for (int i = 0; i < Categories.Length; i++) {
-			int index = i;
-			Categories [i].onClick.AddListener (() => CategoryButton(index));
-		}
-			
-		for (int i = 0; i < Color.Length; i++) {
-			int index = i;
-			Color [i].onClick.AddListener (() => ColorButton(index));
-		}
 
-		// Pega o primeiro e único filho da imagem que é o texto genérico 
-		_textoGenerico = gameObject.transform.GetChild (0).GetComponent<Text>();
-
-
+		
 	}
 	
 	// Update is called once per frame
@@ -48,62 +30,24 @@ public class CustomizePlayer : MonoBehaviour {
 		
 	}
 
-	// Muda os números
-	void UpButton() {
-
-		_NumInText += 1;
-
-		_textoGenerico.text = originalText + " " + _NumInText + " " + _Symbols[_NumInSymbol];
+	// Muda os símbolos
+    void UpButton() {
 		Debug.Log("Up");
 	}
 
-	// Muda os números
+	// Muda os símbolos
 	void DownButton() {
-
-		if (_NumInText > 0) {
-			_NumInText -= 1;
-		}
-
-		_textoGenerico.text = originalText + " " + _NumInText + " " + _Symbols[_NumInSymbol];
 		Debug.Log("Down");
 	}
 
-	// Muda os símbolos
+	// Muda os números
 	void LeftButton() {
-
-		if (_NumInSymbol > 0) {
-			_NumInSymbol -= 1;
-		}
-
-		_textoGenerico.text = originalText + " " + _NumInText + " " + _Symbols[_NumInSymbol];
-
 		Debug.Log("Left");
 	}
 
-	// Muda os símbolos
+	// Muda os números
 	void RightButton() {
-
-		if (_NumInSymbol < 11) {
-			_NumInSymbol += 1;
-		}
-
-		_textoGenerico.text = originalText + " " + _NumInText + " " + _Symbols[_NumInSymbol];
-
 		Debug.Log("Right");
-	}
-
-	// Muda os cor da imagem
-	void CategoryButton(int index) {
-
-		gameObject.GetComponent<Image> ().color = Categories [index].GetComponent<Image> ().color;
-		Debug.Log("Categoria");
-	}
-
-	// Muda os cor do texto
-	void ColorButton(int index) {
-
-		_textoGenerico.color = Color [index].GetComponent<Image> ().color;
-		Debug.Log("Cor");
 	}
 
 }
