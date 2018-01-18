0e6916a15e74ae9a834ccd7ee205b8663e189993
Andre Mazal Krauss <amk1710@gmail.com>
2018-01-18 11:52:08 -0200

    mudanças finais na chain (eu espero)

---
+++ Assets/Scripts/FirstLink.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(DistanceJoint2D))]
+public class FirstLink : MonoBehaviour {
+
+    private Chain c;
+
+    // Use this for initialization
+	void Awake () {
+        c = this.gameObject.GetComponentInParent<Chain>();
+	}
+
+    void OnJointBreak2D(Joint2D brokenJoint)
+    {
+        //Debug.Log("A joint has just been broken!");
+        //Debug.Log("The broken joint exerted a reaction force of " + brokenJoint.reactionForce);
+        //Debug.Log("The broken joint exerted a reaction torque of " + brokenJoint.reactionTorque);
+        if (c != null) c.BreakRope();
+
+    }
+}
