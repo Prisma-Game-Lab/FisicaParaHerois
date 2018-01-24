77a98f42d20e06ed7deb806653903106865e9804
Renan da Fonte <renandafonte@hotmail.com>
2018-01-24 11:03:43 -0200

    Mudando nome de JumpSpeed para JumpForce

--- Assets/Scripts/PlayerInfo.cs
+++ Assets/Scripts/PlayerInfo.cs
@@ -14,8 +14,8 @@ public class PlayerInfo : MonoBehaviour {
     public List<IAction<float>> Actions;
     [Tooltip("Velocidade com que o player se move")]
     public float PaceSpeed = 20.0f;
-    [Tooltip("Velocidade com que o player realiza o pulo")]
-    public float JumpSpeed = 60.0f;
+    [Tooltip("Antigo JumpSpeed. Influencia em velocidade e altura com que o player realiza o pulo")]
+    public float JumpForce = 60.0f;
     [Tooltip("Distância máxima para o player poder usar ações como Push/Pull em um objeto")]
     public float MaxDistanceToNearbyObject = 1.5f;
     public float MaxVelocity = 5.0f;
@@ -178,7 +178,7 @@ public class PlayerInfo : MonoBehaviour {
 
     public void Jump()
     {
-        _rb.AddForce(Vector2.up * JumpSpeed);
+        _rb.AddForce(Vector2.up * JumpForce);
 		AudioSource.PlayClipAtPoint (jump, transform.position);
     }
 
