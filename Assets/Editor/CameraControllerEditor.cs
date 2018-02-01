d2d0f33785cbc17db73bee1a0f6375f9395fc09a
LAB-GRAD\c1410386 <amk1710@gmail.com>
2018-02-01 12:38:44 -0200

    modificaoes na camera, nao terminadas

---
+++ Assets/Editor/CameraControllerEditor.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(CameraController))]
+public class CameraControllerEditor : Editor {
+
+    private float _size = 1.0f;
+    private Vector3 _snap = Vector3.one;
+
+
+
+    public override void OnInspectorGUI()
+    {
+        EditorUtility.SetDirty(target);
+        CameraController c = (CameraController)target;
+        DrawDefaultInspector();
+
+        EditorGUILayout.LabelField("Camera scroll bounds");
+        c.Limit1 = EditorGUILayout.Vector3Field("Limit 1:", c.Limit1);
+        c.Limit2 = EditorGUILayout.Vector3Field("Limit 2:", c.Limit2);
+
+        
+
+        //EditorGUI.BeginChangeCheck();
+        //c.Limit1 = Handles.PositionHandle(c.Limit1, Quaternion.identity);
+        //EditorGUI.EndChangeCheck();
+
+        //EditorGUI.BeginChangeCheck();
+        //Vector3 newL1 = Handles.PositionHandle(c.Limit1, Quaternion.identity);
+        //if (EditorGUI.EndChangeCheck())
+        //{
+        //    Undo.RecordObject(c, "Change bounds");
+        //    c.Limit1 = newL1;
+        //    c.Update();
+        //}
+
+    }
+
+    public void OnSceneGUI()
+    {
+        CameraController c = (CameraController) target;
+        if(c.showBoundingBox) Handles.DrawSolidRectangleWithOutline(new Rect(c.Limit1.x, c.Limit1.y, c.Limit2.x, c.Limit2.y), new Color(0.5f, 0.5f, 0.5f, 0.2f), Color.black);
+    }
+}
--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -9,6 +9,9 @@ public class CameraController : MonoBehaviour {
     public float CameraSpeed = 5f;
     public PostProcessingBehaviour PhysicsVisionPostProcessing;
 
+    public bool disableScroll;
+    public bool showBoundingBox;
+
     void OnValidate()
     {
         if (PhysicsVisionPostProcessing == null)
@@ -17,45 +20,54 @@ public class CameraController : MonoBehaviour {
         }
     }
 
-    [Header("Limits for camera movement")]
-    public Vector2 Limit1;
-    public Vector2 Limit2;
+    [HideInInspector]
+    public Vector3 Limit1;
+    [HideInInspector]
+    public Vector3 Limit2;
+
+    private float _maxX;
+    private float _maxY;
+    private float _minX;
+    private float _minY;
 
 	// Use this for initialization
 	void Start () {
-		
+		//calculate maximum view points
+
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	public void Update () {
         if (CurOffset != Vector3.zero && TimeLeft > 0)
         {
             Vector3 curMove = (Time.deltaTime / TimeLeft) * CurOffset;
+            
+
+            ////limits x axis
+            //if (Camera.main.transform.position.x + curMove.x > Mathf.Max(Limit1.x, Limit2.x))
+            //{
+            //    curMove.x = Mathf.Max(Limit1.x, Limit2.x) - Camera.main.transform.position.x;
+            //}
+            //else if (Camera.main.transform.position.x + curMove.x < Mathf.Min(Limit1.x, Limit2.x))
+            //{
+            //    curMove.x = Mathf.Min(Limit1.x, Limit2.x) - Camera.main.transform.position.x;
+            //}
+
+            ////limits y axis
+            //if (Camera.main.transform.position.y + curMove.y > Mathf.Max(Limit1.y, Limit2.y))
+            //{
+            //    curMove.y = Mathf.Max(Limit1.y, Limit2.y) - Camera.main.transform.position.y;
+            //}
+            //else if (Camera.main.transform.position.y + curMove.y < Mathf.Min(Limit1.y, Limit2.y))
+            //{
+            //    curMove.y = Mathf.Min(Limit1.y, Limit2.y) - Camera.main.transform.position.y;
+            //}
+
             CurOffset -= curMove;
             TimeLeft -= Time.deltaTime;
-
-            //limits x axis
-            if (Camera.main.transform.position.x + curMove.x > Mathf.Max(Limit1.x, Limit2.x))
-            {
-                curMove.x = Mathf.Max(Limit1.x, Limit2.x) - Camera.main.transform.position.x;
-            }
-            else if (Camera.main.transform.position.x + curMove.x < Mathf.Min(Limit1.x, Limit2.x))
-            {
-                curMove.x = Mathf.Min(Limit1.x, Limit2.x) - Camera.main.transform.position.x;
-            }
-
-            //limits y axis
-            if (Camera.main.transform.position.y + curMove.y > Mathf.Max(Limit1.y, Limit2.y))
-            {
-                curMove.y = Mathf.Max(Limit1.y, Limit2.y) - Camera.main.transform.position.y;
-            }
-            else if (Camera.main.transform.position.y + curMove.y < Mathf.Min(Limit1.y, Limit2.y))
-            {
-                curMove.y = Mathf.Min(Limit1.y, Limit2.y) - Camera.main.transform.position.y;
-            }
+            Camera.main.transform.Translate(curMove);
 
             
-            Camera.main.transform.Translate(curMove);
         }
 
         else
@@ -66,6 +78,9 @@ public class CameraController : MonoBehaviour {
 
     public void Move(Vector3 offset)
     {
+        if (disableScroll) return;
+
+        Debug.Log("Move camera. offset: " + offset.ToString());
         CurOffset = offset;
         TimeLeft = 1/CameraSpeed;
     }
