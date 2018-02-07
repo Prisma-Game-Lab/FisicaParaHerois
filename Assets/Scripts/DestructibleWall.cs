5abf15d06e85f4efa148314e6b123ed6df482d04
Renan da Fonte <renandafonte@hotmail.com>
2018-02-07 11:48:46 -0200

    Paredes agora podem ser desativadas pela pressure plate

---
+++ Assets/Scripts/DestructibleWall.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestructibleWall : AffectedByPressurePlate {
+    public List<GameObject> CollidersToDisable; 
+
+	public override void OnPressed () 
+    {
+        SetActive(false);
+	}
+	
+    public override void OnUnpressed()
+    {
+        SetActive(true);	
+	}
+
+    void SetActive(bool status)
+    {
+        foreach (GameObject collider in CollidersToDisable)
+        {
+            collider.SetActive(status);
+        }
+    }
+}
