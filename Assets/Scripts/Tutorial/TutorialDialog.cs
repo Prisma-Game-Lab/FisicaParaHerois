9d63efa067ed41191b8a5500ea843095b8a3df16
Renan <renandafonte@gmail.com>
2018-02-26 11:40:22 -0300

    Script for tutorial dialogs

---
+++ Assets/Scripts/Tutorial/TutorialDialog.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialDialog : MonoBehaviour {
+	private bool _alreadyDisplayedText = false;
+
+	public void OnTriggerStay2D(Collider2D other){
+		if (!_alreadyDisplayedText) {
+			ShowText ();
+		}
+	}
+
+	public void ShowText(){
+		gameObject.SetActive (true);
+		_alreadyDisplayedText = true;
+	}
+
+	public void HideText(){
+		gameObject.SetActive (false);
+	}
+}
+
