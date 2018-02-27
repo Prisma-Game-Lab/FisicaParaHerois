edafe8237cd8164a48f8b49d6289f36122a8a1ae
PC-MATHEUS\Matheus <matheuscpsilvadsg@gmail.com>
2018-02-27 12:17:19 -0300

    Falas do Tutorial 1

--- Assets/Scripts/Tutorial/TutorialDialogTrigger.cs
+++ Assets/Scripts/Tutorial/TutorialDialogTrigger.cs
@@ -4,9 +4,10 @@ using UnityEngine;
 
 public class TutorialDialogTrigger : MonoBehaviour {
 	public TutorialDialog DialogToActivate;
+	public GameObject PlanoFechado;
 
 	// Use this for initialization
 	void OnTriggerStay2D(Collider2D other){
-		DialogToActivate.OnTriggerStay2D(other);
+		DialogToActivate.OnTriggerStay2D(other, PlanoFechado);
 	}
 }
