957f90d6f196fcfca172cbf9342657669cab3545
DannyVegas <thedannyvegas@gmail.com>
2018-03-01 11:32:33 -0300

    Fix na seta de Força. Mapa atualizado, com nova prefab de botão criado, pronta pra atualizar o sprite com fases completas ou não. Animação da minhoca na Splash scene corrigida.

--- Assets/Scripts/Mapa/MapButton.cs
+++ Assets/Scripts/Mapa/MapButton.cs
@@ -5,7 +5,18 @@ using UnityEngine.SceneManagement;
 
 public class MapButton : MonoBehaviour {
     public string SceneToLoad;
-    
+    public bool Completed = false;
+    private Animator Anim;
+
+    private void Start()
+    {
+        Anim = GetComponent<Animator>();
+
+        // Checar se a fase foi completa e atualizar variável "Completed"
+        Anim.SetBool("Completed", Completed);
+
+    }
+
     private void OnMouseDown()
     {
         SceneManager.LoadScene(SceneToLoad);
