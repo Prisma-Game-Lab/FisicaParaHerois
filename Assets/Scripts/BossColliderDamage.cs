3d27c907c72cb4cdf4918ed36a870795f2a94c03
Nelson Donato <nelsinhojd@hotmail.com>
2018-03-01 18:34:39 -0300

    ajustes fases e prefab boss e boss box

---
+++ Assets/Scripts/BossColliderDamage.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossColliderDamage : MonoBehaviour {
+	public Health BossHealth;
+
+	void OnCollisionEnter2D(Collision2D col){
+		if (col.collider.tag == "Player") {
+			BossHealth.TakeDamage (1);
+			Debug.Log ("Player acertou hit no boss");
+		}
+
+	}
+}
